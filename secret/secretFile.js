a7b130e371e298678007af7299535181a1d89216
Abir lal Das <abirdas33@users.noreply.github.com>
2019-07-07 17:08:12 +0530

    third commit

--- secret/secretFile.js
+++ secret/secretFile.js
@@ -1,11 +1,11 @@
 module.exports = {
     facebook: {
-        clientID: '468582293955136',
-        clientSecret: '331078dafd520847ef8d49d1b8755613',
+        clientID: '',
+        clientSecret: '',
     },
     
     google: {
-        clientID: '405451912252-booil2tjp60shkkmovp0bicog796o6fj.apps.googleusercontent.com',
-        clientSecret: 'DTqECg7uZGvL7g0_Za1G2LRv'
+        clientID: '',
+        clientSecret: ''
     }
 }
